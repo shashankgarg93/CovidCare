c2a9d73b14b9c3dc3184ce7bd84dcb923fa00276
Shashank Garg <shashank.garg9300@gmail.com>
2021-05-03 21:39:14 +0530

    added donate_screen and recieve_screen

---
+++ lib/screens/recieve_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class RecieveScreen extends StatefulWidget {
+  @override
+  _RecieveScreenState createState() => _RecieveScreenState();
+}
+
+class _RecieveScreenState extends State<RecieveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
