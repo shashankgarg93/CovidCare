46c3e421e1430ebd34ed486f83c4192d4c3430d2
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2021-05-05 20:38:28 +0530

    bla

--- lib/screens/donate_screen.dart
+++ lib/screens/donate_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:country_state_city_picker/country_state_city_picker.dart';
 
 class DonateScreen extends StatefulWidget {
   @override
@@ -8,119 +7,82 @@ class DonateScreen extends StatefulWidget {
 }
 
 class _DonateScreenState extends State<DonateScreen> {
- String email='';
- String number='';
- String name='';
- String countryValue = '';
- String stateValue = '';
- String cityValue = '';
+  String email = '';
+  String number = '';
+  String name = '';
+  String countryValue = '';
+  String stateValue = '';
+  String cityValue = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-   body: SingleChildScrollView(
-     child: SafeArea(
-       child: Container(
-
-         padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-         child: Column(
-           children: <Widget>[
-             TextFormField(
-autofocus: true,
-               decoration: InputDecoration(
-                 hintText: 'NAME',
-
-enabledBorder: OutlineInputBorder(
-  borderSide: BorderSide(
-        color: Color.fromRGBO(39,33,60,1),
-        width:1.0,
-  ),
-),
-               ),
-
-               onChanged: (val){
-                 setState(() {
-                   name=val;
-
-                 }
-
-                 );
-
-                 },
-
-             ),
-             SizedBox(height:15),
-             TextFormField(
-               autofocus: true,
-               decoration: InputDecoration(
-                 hintText: 'Email',
-enabledBorder: OutlineInputBorder(
- borderSide: BorderSide(
-     color: Color.fromRGBO(39,33,60,1),
-width: 0.5
- )
-)
-               ),
-
-               onChanged: (val){
-                 setState(() {
-                   email=val;
-                 }
-
-                 );
-
-               },
-
-
-             ),
-SizedBox(height: 15,),
-             TextFormField(
-               autofocus: true,
-               decoration: InputDecoration(
-                 hintText: 'Phone Number',
-enabledBorder: OutlineInputBorder(
-  borderSide: BorderSide(
-      color: Color.fromRGBO(39,33,60,1),
-width: 0.5
-  )
-)
-               ),
-
-               onChanged: (val){
-                 setState(() {
-                   number=val;
-                 }
-
-                 );
-
-               },
-
-           ),
-           SelectState(
-                onCountryChanged: (value) {
-                setState(() {
-                  countryValue = value;
-                });
-              },
-              onStateChanged:(value) {
-                setState(() {
-                  stateValue = value;
-                });
-              },
-               onCityChanged:(value) {
-                setState(() {
-                  cityValue = value;
-                });
-              },
-              
-              ),
-           SizedBox(height:10),
-           Center(child: RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.add), label:Text('Submit')))
-         ],
-       ),
-     ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: 'NAME',
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(39, 33, 60, 1),
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  onChanged: (val) {
+                    setState(() {
+                      name = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      hintText: 'Email',
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(39, 33, 60, 1),
+                              width: 0.5))),
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      hintText: 'Phone Number',
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(39, 33, 60, 1),
+                              width: 0.5))),
+                  onChanged: (val) {
+                    setState(() {
+                      number = val;
+                    });
+                  },
+                ),
+                SizedBox(height: 10),
+                Center(
+                    child: RaisedButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.add),
+                        label: Text('Submit')))
+              ],
+            ),
+          ),
+        ),
       ),
-   ),);
-
+    );
   }
 }
--- lib/screens/recieve_screen.dart
+++ lib/screens/recieve_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:covid_care/Services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:enhanced_drop_down/enhanced_drop_down.dart';
+
 class RecieveScreen extends StatefulWidget {
   @override
   _RecieveScreenState createState() => _RecieveScreenState();
@@ -12,62 +13,48 @@ class _RecieveScreenState extends State<RecieveScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-          decoration: BoxDecoration(image:DecorationImage(
-              image: NetworkImage('https://source.unsplash.com/1600x900/?medical'),
-              fit: BoxFit.cover),),
-
-child: Scaffold(
-  appBar: AppBar(
-  ),
-  body: SingleChildScrollView
-  (child: Container(
-  child: SizedBox(
-    height: 200,
-      child: Column(children: [
-        StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('root').snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-      var doc = snapshot.data!.docs;
-      return Expanded(
-              child: new ListView.builder(
-          scrollDirection: Axis.vertical,
-            itemCount: doc.length,
-            itemBuilder: (context, index) {
-
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-
-                      child: 
-                        Text(doc[index].id,
-                        style: TextStyle(fontSize: 30),),
-                        
-
-                  ),
-                );
-            }),
-      );
-            } else {
-      return LinearProgressIndicator();
-            }
-          },
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image:
+                NetworkImage('https://source.unsplash.com/1600x900/?medical'),
+            fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('root').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var doc = snapshot.data!.docs;
+                return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: doc.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          child: Text(
+                            doc[index].id,
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      );
+                    });
+              } else {
+                return LinearProgressIndicator();
+              }
+            },
+          ),
+          // EnhancedDropDown.withData(
+          //           dropdownLabelTitle: "Please select your city if *Available",
+          //           dataSource: AddUser().getcity(x),
+          //           defaultOptionText: "City",
+          //           valueReturned: (chosen) {
+          //              print(chosen);
+          //          })
         ),
-        // EnhancedDropDown.withData(
-        //           dropdownLabelTitle: "Please select your city if *Available",
-        //           dataSource: AddUser().getcity(x),
-        //           defaultOptionText: "City",
-        //           valueReturned: (chosen) {
-        //              print(chosen);
-        //          })
-    ],),
-  ),
-    ),
-  ),
-),
-
+      ),
     );
-
-
   }
 }
